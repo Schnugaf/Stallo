6584d631228ea43b6fb2ff72ee07beb81e5ceb21
Comandzap <andy528200@gmail.com>
2017-01-09 16:44:18 +0100

    no message

---
+++ Source/Stallo/StalloGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "StalloGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STALLO_API AStalloGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
