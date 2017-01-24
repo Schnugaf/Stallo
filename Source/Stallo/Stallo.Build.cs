09cfbfeac1b3e873533912d6af3e23303416ff23
Comandzap <andy528200@gmail.com>
2017-01-24 16:25:41 +0100

    Senses to AI

--- Source/Stallo/Stallo.Build.cs
+++ Source/Stallo/Stallo.Build.cs
@@ -6,7 +6,7 @@ public class Stallo : ModuleRules
 {
 	public Stallo(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","AIModule","GameplayTasks" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
